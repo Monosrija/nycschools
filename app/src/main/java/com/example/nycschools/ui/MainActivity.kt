3e57b9fe3b73cbdb4b0774c899dc576052deffed
Monosrija Maity <mmaity1@asu.edu>
2023-02-07 21:41:25 -0800

    Adding resource files

---
+++ app/src/main/java/com/example/nycschools/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.nycschools.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.nycschools.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupToolbar()
+    }
+
+    private fun setupToolbar(){
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "List of NYC Schools"; // set Title for Toolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
