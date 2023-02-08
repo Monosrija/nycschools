8dfb849519e4791a15c963342eec2646782c973e
Monosrija Maity <mmaity1@asu.edu>
2023-02-07 19:24:35 -0800

    Adding Hilt for dependency injection and Usecase

---
+++ app/src/main/java/com/example/nycschools/AppModule.kt
@@ -0,0 +1,41 @@
+package com.example.nycschools
+
+import com.example.nycschools.domain.network.NycSchoolsApiService
+import com.example.nycschools.domain.SchoolUsecase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideUsecase() : SchoolUsecase {
+        return SchoolUsecase(provideApiService(provideRetrofit(BASE_URL = baseUrl)))
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(BASE_URL:String): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): NycSchoolsApiService = retrofit.create(
+        NycSchoolsApiService::class.java)
+
+    companion object {
+        const val baseUrl =
+            "https://data.cityofnewyork.us/resource/"
+    }
+
+}
---
+++ app/src/main/java/com/example/nycschools/domain/SchoolUsecase.kt
@@ -0,0 +1,20 @@
+package com.example.nycschools.domain
+
+import com.example.nycschools.domain.network.NycSchoolsApiService
+import com.example.nycschools.domain.data.SatDetails
+import com.example.nycschools.domain.data.SchoolInfo
+import javax.inject.Inject
+
+class SchoolUsecase @Inject constructor(
+    private val apiService: NycSchoolsApiService
+) : NycSchoolsApiService {
+
+    override suspend fun getSchoolList(): List<SchoolInfo> {
+        return apiService.getSchoolList()
+            .sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.schoolName })
+    }
+
+    override suspend fun getSatDetails(schoolId: String): List<SatDetails> {
+        return apiService.getSatDetails(schoolId)
+    }
+}
